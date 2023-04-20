b05a4cd50ece3a43ffc70104235e6d538e20cf04
Moises de Paulo Dias <moisesdepaulodias@gmail.com>
2023-04-20 08:50:44 -0300

    Feat: Add sockets lib and a sample code.

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
 from flask_cors import CORS
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 cors = CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('mousemove')
+def handle_mouse_move(data):
+    print(data)  # Do something with the mouse position data received from the frontend
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    socketio.run(app, debug=True, host="0.0.0.0")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
   const [message, setMessage] = useState("");
@@ -7,6 +8,22 @@ function App() {
     fetch("http://localhost:5000/")
       .then(res => res.text())
       .then(setMessage);
+  }, []);
+
+  useEffect(() => {
+    const socket = io('http://localhost:5000');
+    
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      socket.emit('mousemove', { x: clientX, y: clientY });
+    };
+    
+    window.addEventListener('mousemove', handleMouseMove);
+    
+    return () => {
+      socket.disconnect();
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
   }, []);
 
   return (
