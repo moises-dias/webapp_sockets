a6790bf1dc6baa350b51a33310fdba3aa4c963a2
Moises de Paulo Dias <moisesdepaulodias@gmail.com>
2023-04-23 14:42:18 -0300

    Feat: Update application to handle user moving in a canvas.

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,57 @@
 from flask import Flask, request
 from flask_socketio import SocketIO, emit
 
+def print_green(text):
+    print(f'\033[1;32m{text}\033[0m')
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 users = []
-points = []
 
-@app.route('/')
-def index():
+@socketio.on('connect')
+def handle_connect():
     global users
-    users.append(request.sid)
+    name = request.args.get('name')
+    new_user = {'id': request.sid, 'user': name, 'x': 0, 'y': 0}
+    users.append(new_user)
+    emit('update_users', users, broadcast=True)
+    print_green(f"A client with id {request.sid} connected with name {name}")
 
-@socketio.on('click')
-def handle_click(data):
-    global points
-    points.append((request.sid, data['x'], data['y']))
-    emit('new_point', {'user': request.sid, 'x': data['x'], 'y': data['y']}, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global users
+    # TODO improve this logic
+    users = [usr for usr in users if usr['id'] != request.sid]
+    emit('update_users', users, broadcast=True)
+    print_green(f"{request.sid} disconnected")
+
+@socketio.on('move')
+def handle_move(data):
+    # TODO frontend should just pass "moving up" once
+    # and backend should notify everybody, this way there
+    # is no need to a socket message for every 10 pixels
+    # the user will move until backend get movement stop
+    # but how to get colision using this logic?
+    # TODO hide user id from frontend
+    print_green(data['direction'])
+    print_green(request.sid)
+    # TODO improve this logic
+    for usr in users:
+        if usr['id'] == request.sid:
+            if data['direction'] == 87: # w
+                usr['y'] -= 10
+            elif data['direction'] == 83: # s
+                usr['y'] += 10
+            elif data['direction'] == 65: # a
+                usr['x'] -= 10
+            elif data['direction'] == 68: # d
+                usr['x'] += 10
+            
+            emit('update_users', users, broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
+const CANVAS_WIDTH = 400;
+const CANVAS_HEIGHT = 400;
+const CANVAS_BORDER_STYLE = '1px solid black';
+const CIRCLE_RADIUS = 50;
+const MOVEMENT_KEYS = [87, 83, 65, 68];
+
 function App() {
-  const [message, setMessage] = useState("");
   const [socket, setSocket] = useState(null);
+  const [users, setUsers] = useState([]);
+  const canvasRef = useRef(null);
+
+  const userName = (Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000).toString();
+
+  const drawCanvas = (context, users) => {
+    context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    for (let i = 0; i < users.length; i++) {
+      context.beginPath();
+      context.arc(users[i].x, users[i].y, CIRCLE_RADIUS, 0, 2 * Math.PI, false);
+      context.fillStyle = '#FF4136';
+      context.fill();
+      context.fillStyle = '#000';
+      context.font = 'bold 20px sans-serif';
+      context.fillText(users[i].user, users[i].x - 20, users[i].y + 5);
+    }
+  };
 
   useEffect(() => {
-    const newSocket = io('http://192.168.59.100:30886');
+    const newSocket = io('http://localhost:5000', { query: { name: userName} });
     setSocket(newSocket);
     return () => newSocket.disconnect();
   }, []);
@@ -14,25 +36,42 @@ function App() {
   useEffect(() => {
     if (!socket) return;
 
-    socket.on('new_point', data => {
-      console.log(data);
-      setMessage(`User ${data.user} clicked on (${data.x}, ${data.y})`);
+    socket.on('update_users', users => {
+      setUsers(users);
+      console.log(users);
     });
 
-    return () => socket.off('new_point');
+    return () => {
+      socket.off('update_users');
+    };
   }, [socket]);
 
-  const handleClick = (e) => {
+  useEffect(() => {
     if (!socket) return;
 
-    const position = { x: e.clientX, y: e.clientY };
-    socket.emit('click', position);
-  };
+    const handleKeyDown = (event) => {
+      if (MOVEMENT_KEYS.includes(event.keyCode)) {
+        socket.emit('move', { user: userName, direction: event.keyCode });
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    drawCanvas(context, users);
+    return () => {};
+  }, [users]);
 
   return (
-    <div onClick={handleClick}>
-      <h1>Hello World!!!</h1>
-      <p>{message}</p>
+    <div>
+      <canvas ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT} style={{ border: CANVAS_BORDER_STYLE }}></canvas>
     </div>
   );
 }
